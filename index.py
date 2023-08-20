57e565975f0e344482f295771308d6e8ab3bb27d
kimminam <bluemind8520@gmail.com>
2023-08-20 09:12:29 +0900

    test

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://vibe.naver.com/chart/total'
+url = 'https://www.youtube.com/watch?v=Kr_5AgDx1f8'
 res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "html")
-songs = soup.find_all(attrs={"class":"inner_cell"})
 
-for song in songs :
-    print(songs.get_text)
+print(soup.find("yt-formatted-string",attrs={"class":"style-scope ytd-watch-metadata"}).getText())
 # vibe melon
