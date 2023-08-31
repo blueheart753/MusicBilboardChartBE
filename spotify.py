250ac2e3e29a5faca4fcb52882ae1bc4bc6ee176
kimminam <bluemind8520@gmail.com>
2023-08-31 11:01:18 +0900

    add spotify

---
+++ spotify.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://open.spotify.com/playlist/37i9dQZEVXbNxXF4SkHj9F")
+
+
+def songsName() :
+    songNames = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,".t_yrXoUO3qGsJS4Y6iXX")))
+
+    for song in songNames:
+        print(song.text,end='\n')
+
+def artist() :
+    artist = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,".rq2VQ5mb9SDAFWbBIUIn")))
+    for Artists in artist:
+        print(Artists.text,end='\n')
+
+def count() :
+    count = 1
+    for countUp in range(100):
+        countUp+=count
+        print(countUp)
+songsName()
+artist()
+# count()
+
+
+time.sleep(0.05)
+
+driver.quit()
